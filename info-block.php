93cd854812a657a7e10e2e4eacf4bdb1e3b2ecc9
cmsExpertss <cmsexpertss@gmail.com>
2024-07-05 00:19:17 +0500

    release

--- info-block.php
+++ info-block.php
@@ -1,21 +1,40 @@
 <?php
+
 /**
  * Plugin Name:       Info Block
- * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.1
- * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Description:       Notifications Block (Info, Tip, Warning, Error)
+ * Requires at least: 6.3.0
+ * Requires PHP:      7.4
+ * Version:           0.0.1
+ * Author:            admin
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       info-block
- *
- * @package CreateBlock
+ * Text Domain:       info_block
+ * Website:           
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$plugin_prefix = 'INFOBLOCK';
+
+define($plugin_prefix . '_DIR', plugin_basename(__DIR__));
+define($plugin_prefix . '_BASE', plugin_basename(__FILE__));
+define($plugin_prefix . '_PATH', plugin_dir_path(__FILE__));
+define($plugin_prefix . '_VER', '0.0.1');
+define($plugin_prefix . '_CACHE_KEY', 'info_block-cache-key-for-plugin');
+define($plugin_prefix . '_REMOTE_URL', 'https://local-dplugins.local/wp-content/uploads/downloads/25/info.json');
+
+require constant($plugin_prefix . '_PATH') . 'inc/update.php';
+
+new DPUpdateChecker(
+	constant($plugin_prefix . '_DIR'),
+	constant($plugin_prefix . '_VER'),
+	constant($plugin_prefix . '_CACHE_KEY'),
+	constant($plugin_prefix . '_REMOTE_URL'),
+	constant($plugin_prefix . '_BASE')
+);
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
